88a9af71c020863fb328727b6ac0631b048f4891
StaeyKay <ekwashie001@st.ug.edu.gh>
2025-02-13 15:18:26 +0000

    updated the base url

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5050/api/v1";
+const BASE_URL = "https://quiz-api-yk9k.onrender.com/api/v1";
 
 // Save session data in local storage
 export function saveSession(playerName) {
