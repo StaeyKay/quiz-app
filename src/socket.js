aebf93582afcb8ab41127c15184c76e32e969a0f
StaeyKay <ekwashie001@st.ug.edu.gh>
2025-02-27 21:35:21 +0000

    updated the base url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5050", { autoConnect: false });
+const socket = io("https://quiz-api-yk9k.onrender.com", { autoConnect: false });
 
 export default socket;
